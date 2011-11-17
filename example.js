d1655edf3668a0774044082a565f1f77a4edce0f
Mathias Buus <mathiasbuus@gmail.com>
2011-11-17 13:06:04 +0100

    Updated example.js

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-require('xsockets').listen(11000, function(socket) {
+require('json-sockets').listen(11000, function(socket) {
 	socket.on('message', function(message) {
 		console.log('rcvd', message);
 		socket.send(message);
